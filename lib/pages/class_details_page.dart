d5a46f81ac118104161d0fa145ec2939296231ca
HariHar <sanjaytataria@gmail.com>
2023-10-27 20:57:28 +0530

    annoucement ready

--- lib/pages/class_details_page.dart
+++ lib/pages/class_details_page.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../auth.dart';
 import '../models/class.dart';
 import '../models/announcement.dart';
+import 'new_announcement.dart';
 
 
 class ClassDetailsPage extends StatefulWidget {
@@ -44,12 +49,42 @@ class _ClassDetailsPageState extends State<ClassDetailsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.currentClass.className),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 1,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NewAnnouncementPage(className: widget.currentClass.className)),
+                    );
+                  },
+                  child: Text('New Announcement'),
+                ),
+              ),
+              PopupMenuItem(
+                value: 2,
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text('New Material'),
+                ),
+              ),
+              PopupMenuItem(
+                value: 3,
+                child: ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text('New Assignment'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       body: PageView(
         controller: _pageController,
@@ -59,7 +94,7 @@ class _ClassDetailsPageState extends State<ClassDetailsPage> {
           });
         },
         children: [
-          StreamSection(),
+          StreamSection(className: widget.currentClass.className),
           ClassSection(title: 'Classwork'),
           PeopleSection(),
         ],
@@ -87,109 +122,222 @@ class _ClassDetailsPageState extends State<ClassDetailsPage> {
 }
 
 
+
 class ClassSection extends StatelessWidget {
   final String title;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
 
-  ClassSection({required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        title,
-        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-      ),
-    );
+  ClassSection({required this.title}) {
+    // Initialize the notification plugin when the ClassSection is created.
+    var androidInitialize = AndroidInitializationSettings('app_icon');
+    var initializationSettings =
+    InitializationSettings(android: androidInitialize);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
-}
 
-class StreamSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Stack(
       children: [
-        _buildAnnouncementCard(
-          'Important Announcement',
-          'There will be a quiz on Monday. Be prepared!',
+        Center(
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
         ),
-        _buildAssignmentCard(
-          'Assignment 1',
-          'Complete the exercises on pages 10-15.',
-        ),
-        _buildMaterialCard(
-          'Study Materials',
-          'Download the PDF notes for this week.',
-        ),
-        _buildPDFCard(
-          'Sample PDF',
-          'Sample PDF Document',
-          'https://example.com/sample.pdf', // Replace with the actual PDF URL
+        Positioned(
+          bottom: 16.0, // Adjust the position as needed
+          right: 16.0, // Adjust the position as needed
+          child: _buildPopupMenuButton(),
         ),
       ],
     );
   }
 
-  Widget _buildAnnouncementCard(String title, String content) {
-    return Card(
-      elevation: 3.0,
-      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(content),
-      ),
-    );
-  }
-
-  Widget _buildAssignmentCard(String title, String content) {
-    return Card(
-      elevation: 3.0,
-      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(content),
-      ),
-    );
-  }
-
-  Widget _buildMaterialCard(String title, String content) {
-    return Card(
-      elevation: 3.0,
-      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(content),
-      ),
-    );
-  }
-
-  Widget _buildPDFCard(String title, String description, String pdfUrl) {
-    return Card(
-      elevation: 3.0,
-      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(description),
-        onTap: () {
-          // Implement opening PDF here (e.g., using a PDF viewer package)
-          // You can use the pdfUrl to load and display the PDF.
-          // Replace this with your PDF viewing logic.
+  Widget _buildPopupMenuButton() {
+    return ElevatedButton(
+        onPressed: () async {
+          // await _showNotification('Hari Testing', 'my testin my testing 123... 123');
         },
-      ),
+        child: Text('Click to notify.')
+    );
+  }
+
+
+//
+//
+//   Future<void> _showNotification(String title, String content) async {
+//
+//
+//     var androidDetails = AndroidNotificationDetails(
+//         'channel_id', 'channel_name',
+//         importance: Importance.max, priority: Priority.high);
+//     var platformChannelSpecifics =
+//     NotificationDetails(android: androidDetails);
+//
+//     await flutterLocalNotificationsPlugin.show(
+//       0,
+//       title,
+//       content,
+//       platformChannelSpecifics,
+//     );
+//   }
+// }
+
+}
+
+class StreamSection extends StatelessWidget {
+  late final String className;
+  StreamSection({required this.className});
+
+  final User? currentUser = FirebaseAuth.instance.currentUser; // Get the current user
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('classes').doc(className).collection('announcements').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Text('No announcements available');
+        }
+
+        List<DocumentSnapshot> announcements = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: announcements.length,
+          itemBuilder: (context, index) {
+            var announcement = announcements[index].data() as Map<String, dynamic>;
+            var text = announcement['title'];
+            var body = announcement['announcement'];
+            var timestamp = announcement['timestamp'];
+            var createdBy = announcement['createdBy'];
+
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.green,
+                child: Icon(Icons.person),
+              ), // Replace PhotoWidget with your widget
+              title: Text(text ?? "no title"),
+              subtitle: Text('Timestamp: $timestamp'),
+              trailing: (currentUser != null && currentUser?.email == createdBy)
+                  ? Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      // Handle update here
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // Handle delete here
+                    },
+                  ),
+                ],
+              ) : null,
+              onTap: () {
+                // Handle the announcement's body here
+              },
+            );
+          },
+        );
+      },
     );
   }
 }
+//
+//
+// // _buildAnnouncementCard(
+// // 'Important Announcement',
+// // 'There will be a quiz on Monday. Be prepared!',
+// // ),
+// // _buildAssignmentCard(
+// // 'Assignment 1',
+// // 'Complete the exercises on pages 10-15.',
+// // ),
+// // _buildMaterialCard(
+// // 'Study Materials',
+// // 'Download the PDF notes for this week.',
+// // ),
+// // _buildPDFCard(
+// // 'Sample PDF',
+// // 'Sample PDF Document',
+// // 'https://example.com/sample.pdf', // Replace with the actual PDF URL
+// // ),
+//
+//
+// Widget _buildAnnouncementCard(String title, String content) {
+//   return Card(
+//     elevation: 3.0,
+//     margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//     child: ListTile(
+//       title: Text(
+//         title,
+//         style: TextStyle(fontWeight: FontWeight.bold),
+//       ),
+//       subtitle: Text(content),
+//     ),
+//   );
+// }
+//
+// Widget _buildAssignmentCard(String title, String content) {
+//   return Card(
+//     elevation: 3.0,
+//     margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//     child: ListTile(
+//       title: Text(
+//         title,
+//         style: TextStyle(fontWeight: FontWeight.bold),
+//       ),
+//       subtitle: Text(content),
+//     ),
+//   );
+// }
+//
+// Widget _buildMaterialCard(String title, String content) {
+//   return Card(
+//     elevation: 3.0,
+//     margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//     child: ListTile(
+//       title: Text(
+//         title,
+//         style: TextStyle(fontWeight: FontWeight.bold),
+//       ),
+//       subtitle: Text(content),
+//     ),
+//   );
+// }
+//
+// Widget _buildPDFCard(String title, String description, String pdfUrl) {
+//   return Card(
+//     elevation: 3.0,
+//     margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//     child: ListTile(
+//       title: Text(
+//         title,
+//         style: TextStyle(fontWeight: FontWeight.bold),
+//       ),
+//       subtitle: Text(description),
+//       onTap: () {
+//         // Implement opening PDF here (e.g., using a PDF viewer package)
+//         // You can use the pdfUrl to load and display the PDF.
+//         // Replace this with your PDF viewing logic.
+//       },
+//     ),
+//   );
+// }
+//
+//
+//
+//
+//
+
 
 class PeopleSection extends StatefulWidget {
   const PeopleSection({super.key});
